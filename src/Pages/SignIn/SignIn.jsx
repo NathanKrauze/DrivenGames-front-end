c138f6ead5fbfa53aba968c8ab387be1ff608de7
Mateus Faissal <mateusfaissal@gmail.com>
2023-09-05 10:59:34 -0300

    feat: sign-up and sign-in done

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,5 +1,113 @@
-export default function SignIn(){
-    return(
-        <></>
+import { styled } from "styled-components"
+import axios from "axios"
+import { Link, useNavigate } from "react-router-dom"
+import { useState, useContext } from "react"
+import LogoDrivenGames from "../../Assets/LogoDrivenGames.png"
+import tokenContext from "../../contexts/TokenContext"
+
+
+export default function SignIn() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [token, setToken] = useContext(tokenContext);
+    const navigate = useNavigate();
+
+    function handleToken(dbToken) {
+
+        const newToken = dbToken;
+        setToken(newToken);
+        localStorage.setItem('token', newToken);
+        navigate('/home');
+    }
+
+    function signIn(e) {
+
+        e.preventDefault();
+
+        axios.post(`${import.meta.env.VITE_API_URL}/sign-in`, { email, password })
+            .then((res) => {
+                handleToken(res.data)
+            })
+            .catch((err) => alert(err.message))
+
+    }
+
+
+    return (
+        <SignInContainer>
+            <Form onSubmit={signIn}>
+                <Img src={LogoDrivenGames} />
+                <Input data-test="email" placeholder="E-mail" type="email" value={email} onChange={e => setEmail(e.target.value)} required />
+                <Input data-test="password" placeholder="Senha" type="password" value={password} onChange={e => setPassword(e.target.value)} required />
+                <Button data-test="sign-in-submit">Entrar</Button>
+            </Form>
+
+            <Link to={'/cadastro'}>
+                <p> Primeira vez? Cadastre-se! </p>
+            </Link>
+        </SignInContainer>
     )
 }
+
+const SignInContainer = styled.section`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  p {
+    margin-top: 40px;
+    font-family: 'Rajdhani', sans-serif;
+    color: white;
+    text-decoration: none;
+  }
+`
+const Form = styled.form`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 15px;
+        width: 50%;
+        border-radius: 5px;
+`
+
+const Input = styled.input`
+        font-size: 20px;
+        width: calc(50% - 30px);
+        border-radius: 5px;
+        outline: none;
+        border: 1px solid #ccc;
+        padding: 15px;
+        margin: 1px;
+        font-family: 'Rajdhani', sans-serif;
+            :focus {
+                border: 2px solid #ffb6b6;
+                margin: 0px;
+            }
+
+`
+const Button = styled.button`
+
+        outline: none;
+        border: none;
+        border-radius: 5px;
+        background-color: #CA24B6;
+        font-size: 20px;
+        font-weight: 600;
+        color: #fff;
+        cursor: pointer;
+        width: 50%;
+        padding: 12px;
+        font-family: 'Rajdhani', sans-serif;
+
+`
+
+const Img = styled.img`
+
+        width: 200px;
+        height: 200px;
+
+
+`
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,5 +1,103 @@
-export default function SignUp(){
-    return(
-        <></>
+import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react"
+import styled from "styled-components"
+import axios from "axios"
+import LogoDrivenGames from "../../Assets/LogoDrivenGames.png"
+export default function SignUp() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confPassword, setConfPassword] = useState("");
+    const navigate = useNavigate();
+
+    function signUp(e) {
+
+        e.preventDefault();
+
+        if (password !== confPassword) return alert("Password must be equal!");
+
+        axios.post(`${import.meta.env.VITE_API_URL}/sign-up`, { name, email, password })
+            .then(() => navigate("/"))
+            .catch((err) => alert(err.message))
+
+    }
+    return (
+        <SignUpContainer>
+            <Form onSubmit={signUp}>
+                <Img src={LogoDrivenGames} />
+                <Input data-test="name" placeholder="Nome" type="text" value={name} onChange={e => setName(e.target.value)} required />
+                <Input data-test="email" placeholder="E-mail" type="email" value={email} onChange={e => setEmail(e.target.value)} required />
+                <Input data-test="password" placeholder="Senha" type="password" value={password} onChange={e => setPassword(e.target.value)} required />
+                <Input data-test="conf-password" placeholder="Confirme a senha" type="password" value={confPassword} onChange={e => setConfPassword(e.target.value)} required />
+                <Button data-test="sign-up-submit">Cadastrar</Button>
+            </Form>
+
+            <Link to={"/home"}>
+                <p> Já tem uma conta? Entre agora! </p>
+            </Link>
+        </SignUpContainer>
     )
 }
+
+const SignUpContainer = styled.section`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  p {
+    margin-top: 40px;
+    font-family: 'Rajdhani', sans-serif;
+    color: white;
+    text-decoration: none;
+  }
+`
+const Form = styled.form`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 15px;
+        width: 50%;
+        border-radius: 5px;
+`
+
+const Input = styled.input`
+        font-size: 20px;
+        width: calc(50% - 30px);
+        border-radius: 5px;
+        outline: none;
+        border: 1px solid #ccc;
+        padding: 15px;
+        margin: 1px;
+        font-family: 'Rajdhani', sans-serif;
+            :focus {
+                border: 2px solid #ffb6b6;
+                margin: 0px;
+            }
+
+`
+const Button = styled.button`
+
+        outline: none;
+        border: none;
+        border-radius: 5px;
+        background-color: #CA24B6;
+        font-size: 20px;
+        font-weight: 600;
+        color: #fff;
+        cursor: pointer;
+        width: 50%;
+        padding: 12px;
+        font-family: 'Rajdhani', sans-serif;
+
+`
+
+const Img = styled.img`
+
+        width: 200px;
+        height: 200px;
+
+
+`
